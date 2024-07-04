8fa0005677a9c1137b925ff1607873e6ec711c64
Roshniparasharr <139575123+Roshniparasharr@users.noreply.github.com>
2024-07-04 18:11:36 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -34,8 +34,8 @@ function App() {
       <div className={`container my-3 ${mode === 'dark' ? 'bg-dark text-light' : 'bg-light text-dark'}`}>
         <Routes>
           {/* Set default route to render TextForm component */}
-          <Route path="/TextWiz" element={<TextForm showAlert={showAlert} heading="Try TextWiz - Word Counter, Character Counter, Remove Extra Spaces" mode={mode} />} />
-          <Route path="/TextWiz/about" element={<About mode={mode} />} />
+          <Route path="/TextWiz-React" element={<TextForm showAlert={showAlert} heading="Try TextWiz - Word Counter, Character Counter, Remove Extra Spaces" mode={mode} />} />
+          <Route path="/TextWiz-React/about" element={<About mode={mode} />} />
         </Routes>
       </div>
     </Router>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,17 +5,17 @@ export default function NavBar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/TextWiz" style={{fontFamily:'monospace'}}><strong>TextWiz</strong></Link>
+        <Link className="navbar-brand" to="/TextWiz-React" style={{fontFamily:'monospace'}}><strong>TextWiz</strong></Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to="/TextWiz" style={{fontFamily:'monospace'}}>Home</Link>
+              <Link className="nav-link" aria-current="page" to="/TextWiz-React" style={{fontFamily:'monospace'}}>Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/TextWiz/about" style={{fontFamily:'monospace'}}>About</Link>
+              <Link className="nav-link" to="/TextWiz-React/about" style={{fontFamily:'monospace'}}>About</Link>
             </li>
           </ul>
           <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`} style={{fontFamily:'monospace'}}>
